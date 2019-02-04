ba45f8da04746746ef6f7f7f9fda73263990d45a
MiguelARam27 <garandster27@live.com>
2019-02-04 18:24:57 -0500

    Added Menu
    Fixed Menu issues

---
+++ Senior Design/menu/menu.js
@@ -0,0 +1,30 @@
+let menu_icon =document.getElementById('MenuIcon');
+let menu = document.getElementById('MainMenu');
+let close_button= document.getElementById('close');
+
+function showmenu(){
+    menu_icon.style.display="none";
+    menu.style.left='0px';
+    menu.style.clipPath='polygon(0 0,100% 0,100% 100%,0% 100%)';
+}
+
+function hidemenu(){
+    menu_icon.style.display="block";
+    menu.style.left="-300px";
+    menu.style.clipPath="polygon(0 0,0% 0,100% 100%,0% 100%)";
+}
+
+//menu icon clicked to bring up menu
+menu_icon.addEventListener('click',function(){ 
+    setTimeout(showmenu,100);
+});
+
+//"close" img clicked to close menu
+close_button.addEventListener('click',function(){
+    setTimeout(hidemenu,100);
+});
+
+
+
+
+
