2ad99c3f783a120abc00c12e0802beabf8d0e411
MiguelARam27 <garandster27@live.com>
2019-02-05 09:17:40 -0500

    users
    -added a bit of styling to users selections

--- Senior Design/user selection/main.js
+++ Senior Design/user selection/main.js
@@ -1,4 +1,11 @@
-let trial = document.getElementsByClassName("person4")[0];
-trial.addEventListener("click", function(){
-alert("it worked");
-});
+let trial = document.getElementsByClassName("person");
+// trial.addEventListener("click", function(){
+// alert("it worked");
+// });
+
+for (let i = 0; i < trial.length; i++) {
+    trial[i].addEventListener("click", function() {
+      alert("clicked");
+    });
+  }
+  
