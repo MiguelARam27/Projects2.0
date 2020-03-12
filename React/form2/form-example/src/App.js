55025fb8ef3b37dbdb129cf61ece797b518027c2
MiguelARam27 <garandster27@live.com>
2020-03-11 23:59:03 -0400

    transaction form
    -global state added

--- React/form2/form-example/src/App.js
+++ React/form2/form-example/src/App.js
@@ -5,21 +5,23 @@ import {IncomeExpenses} from './components/IncomeExpenses'
 import {TransactionList} from './components/TransactionList'
 import {AddTransaction} from './components/AddTransaction'
 
+
+import { GlobalProvider } from './context/GlobalState'
 import './App.css' 
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-     <div className="container">
-       <Balance/>
-       <IncomeExpenses/>
-       <TransactionList/>
-     </div>
-      <AddTransaction/>
-      
-    
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <Header></Header>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+      </div>
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
--- React/form2/form-example/src/context/GlobalState.js
+++ React/form2/form-example/src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, {createContext, useReducer} from 'react'
-
+import AppReducer from './AppReducer'
 
 //initial state
 
@@ -11,3 +11,35 @@ const initialState = {
         { id: 4, text: 'Camera', amount: 150 }
     ]
 }
+
+
+//create context
+ export const GlobalContext = createContext( initialState );
+
+ //provider compononent
+
+ export const GlobalProvider = ({ children} ) =>{
+    const [state, dispatch] = useReducer(AppReducer,initialState);
+
+    //actions
+    function deleteTransaction(id) {
+        dispatch({
+            type:'DELETE_TRANSACTION',
+            payload:id
+        })
+    }
+    function addTransaction(transaction) {
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload:transaction
+        })
+    }
+
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+ }
