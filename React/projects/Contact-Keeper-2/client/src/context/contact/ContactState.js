d843eb484b5bfacedd85aa1e733cefff46d46ac8
MiguelARam27 <garandster27@live.com>
2020-03-27 18:34:35 -0400

    logger project started

--- React/projects/Contact-Keeper-2/client/src/context/contact/ContactState.js
+++ React/projects/Contact-Keeper-2/client/src/context/contact/ContactState.js
@@ -72,9 +72,9 @@ const ContactState = props => {
   // Delete Contact
   const deleteContact = async (id) => {
     try {
-      const res = await axios.delete('/api/contacts');
+      const res = await axios.delete(`/api/contacts/${id}`);
       dispatch({
-        type:ADD_CONTACT,
+        type:DELETE_CONTACT,
         payload:res.data
       });
     } catch (err) {
@@ -87,6 +87,30 @@ const ContactState = props => {
 
   };
 
+   // Update Contact
+   const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`,contact,config);
+      dispatch({
+        type:UPDATE_CONTACT,
+        payload:res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload:err.response.msg
+      });
+    }
+
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+
   // Set Current Contact
   const setCurrent = contact => {
     dispatch({ type: SET_CURRENT, payload: contact });
@@ -97,10 +121,7 @@ const ContactState = props => {
     dispatch({ type: CLEAR_CURRENT });
   };
 
-  // Update Contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
-  };
+ 
 
   // Filter Contacts
   const filterContacts = text => {
