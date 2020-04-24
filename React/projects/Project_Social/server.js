b1b52b91313d4f0411e930b8e5a7b6f894314407
MiguelARam27 <garandster27@live.com>
2020-04-24 00:16:02 -0400

    Project-social
    Connected to mongoDB Atlas

--- React/projects/Project_Social/server.js
+++ React/projects/Project_Social/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
+const connectDB = require('./config/db')
+
 
 const app = express();
+
+
+//connect to db 
+connectDB();
 
 app.get('/', (req, res) =>
     res.json({ msg: 'Welcome to the Social API...' })
 );
 
 // Define Routes
-
 
 const PORT = process.env.PORT || 5000;
 
