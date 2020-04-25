6695544b4ef0e32ad95bfc6110d0ed3c693b71c1
MiguelARam27 <garandster27@live.com>
2020-04-25 00:04:35 -0400

    Project-social
    -api routes set up
    -user model set up

--- React/projects/Project_Social/server.js
+++ React/projects/Project_Social/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const connectDB = require('./config/db')
 
 
@@ -9,11 +8,13 @@ const app = express();
 //connect to db 
 connectDB();
 
-app.get('/', (req, res) =>
-    res.json({ msg: 'Welcome to the Social API...' })
-);
+app.get('/', (req, res) => res.json('API running'));
 
 // Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profiles'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
