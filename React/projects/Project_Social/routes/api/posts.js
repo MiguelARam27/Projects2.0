3dfa21320caef8341ff24419b10d01633db28f1a
MiguelARam27 <garandster27@live.com>
2020-05-12 18:13:44 -0400

    Project-social
    Post model done
    Post POST route added

--- React/projects/Project_Social/routes/api/posts.js
+++ React/projects/Project_Social/routes/api/posts.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+
+const Post = require('../../models/Post');
+
+
 
 //@route GET api/posts
-router.get('/', (req, res) => res.send('Post router'));
+//@desc create a post
+// @acces Private
+router.get('/', [auth,
+    [check('text', "Text is required").not().isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+});
 
 
 
