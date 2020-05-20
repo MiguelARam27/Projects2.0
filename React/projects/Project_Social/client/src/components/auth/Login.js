1003c8ca792884db8b73449694206e83a71a6ca3
MiguelARam27 <garandster27@live.com>
2020-05-20 00:23:15 -0400

    Project-social
    -added layout components
    -implemented redux
    -added alert state to redux store
    -alert component added
    -alert component working with register

---
+++ React/projects/Project_Social/client/src/components/auth/Login.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from "react-router-dom";
+
+const Login = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    // const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('success');
+    };
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Login</h1>
+            <p className="lead"><i className="fas fa-user"></i> Log in</p>
+            <form className="form" action="create-profile.html" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={onChange}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </Fragment>
+    );
+};
+
+export default Login;
---
+++ React/projects/Project_Social/client/src/components/auth/Register.js
@@ -0,0 +1,86 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
+
+
+const Register = ({ setAlert }) => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    // const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            setAlert('passwords do not match', 'danger');
+        } else {
+            console.log("success");
+        }
+    };
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" action="create-profile.html" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={onChange}
+                        required />
+                </div>
+                <div className="form-group">
+                    <input type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        value={password2}
+                        onChange={onChange}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
+    );
+};
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
