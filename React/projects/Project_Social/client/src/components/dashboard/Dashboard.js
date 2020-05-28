fb55f4cf96b2b9fb07e06b7124b92ad00e12b515
MiguelARam27 <garandster27@live.com>
2020-05-28 00:27:57 -0400

    Project-social
    -Edit profile component added and working
    -Fixed a tag in Profile component to be a Link tag

--- React/projects/Project_Social/client/src/components/dashboard/Dashboard.js
+++ React/projects/Project_Social/client/src/components/dashboard/Dashboard.js
@@ -5,39 +5,50 @@ import { getCurrentProfile } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 
-
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
-
-    useEffect(() => {
-        getCurrentProfile();
-    }, [getCurrentProfile]);
-    return loading && profile === null ? <Spinner /> : <Fragment>
-        <h1 className="large text-primary">Dashboard</h1>
-        <p className="lead">
-            <i className="fas fa-user"></i>
-            Welcome {user && user.name}
-        </p>
-        {profile !== null ? <h1>hello</h1> :
-            <Fragment>
-                <p>
-                    you have not yet set up a profile, please add some info
-                </p>
-                <Link to='/create-profile' className='btn btn-primary my-1'>Create profile</Link>
-            </Fragment>}
-    </Fragment>;
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>
+        Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions></DashboardActions>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>you have not yet set up a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    profile: state.profile
-
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);;
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
