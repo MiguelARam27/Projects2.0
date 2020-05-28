6b8e8abcc353901e17bddfd7e544ca3d5e783162
MiguelARam27 <garandster27@live.com>
2020-05-27 23:55:08 -0400

    Project-social
    -Create profile component implemented
    -create profile for user works

--- React/projects/Project_Social/client/src/components/profile-forms/CreateProfile.js
+++ React/projects/Project_Social/client/src/components/profile-forms/CreateProfile.js
@@ -1,7 +1,10 @@
 import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { createProfile } from '../../actions/profile';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -35,9 +38,13 @@ const CreateProfile = (props) => {
 
   const onchange = (e) => {
     setFormData({
-      ...FormData,
+      ...formData,
       [e.target.name]: e.target.value,
     });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
   };
 
   return (
@@ -48,7 +55,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className='form'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <select name='status' value={status} onChange={onchange}>
             <option value='0'>* Select Professional Status</option>
@@ -212,6 +219,8 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
